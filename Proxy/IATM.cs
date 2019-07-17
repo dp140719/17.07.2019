5e4adaf84afc28904b30b9d15f6dcbfef9f14bdb
dp140719 <52877500+dp140719@users.noreply.github.com>
2019-07-17 13:09:35 +0300

    Add files via upload

---
+++ Proxy/IATM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public interface IATM
+    {
+
+        void Desposit(int amount);
+	
+	    bool Withdraw(int amount);
+	
+	    void InsertCard();
+	
+	    void EjectCard();
+
+        int GetTotalCash();
+
+        string GetStatus();
+	
+    }
+}
---
+++ Proxy/RealATM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class RealATM : IATM
+    {
+
+        protected int totalCash;
+
+        public void Desposit(int amount)
+        {
+            totalCash += amount;
+        }
+
+        public void EjectCard()
+        {
+            Console.WriteLine("Eject card.....");
+        }
+
+        public void InsertCard()
+        {
+            Console.WriteLine("I have a card in my stomach now");
+        }
+
+        public bool Withdraw(int amount)
+        {
+
+            if (amount < totalCash)
+            {
+                totalCash -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetStatus()
+        {
+            return "All good thumbs up!";
+        }
+
+        public int GetTotalCash()
+        {
+            return totalCash;
+        }
+    }
+}
